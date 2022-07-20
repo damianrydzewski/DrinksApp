1a653d85da9fab161479c7bf03ad718a7fa4f4ef
Damian Rydzewski <107407005+damianrydzewski@users.noreply.github.com>
2022-07-20 12:37:04 +0200

    Update and rename DrinkObjectView.swift to DrinkView.swift

---
+++ DrinksApp/Views/DrinkView.swift
@@ -0,0 +1,108 @@
+//
+//  DrinkObjectView.swift
+//  DrinksApp
+//
+//  Created by Damian on 16/07/2022.
+//
+
+import SwiftUI
+
+struct DrinkView: View {
+    @StateObject var drinkDetailVM = DrinkListViewModel()
+    @State private var isSheetDisplayed = false
+    
+    var drinkThumb: URL
+    var drinkName: String
+    var drinkIDs: String
+
+    var body: some View {
+        Button {
+            isSheetDisplayed.toggle()
+        } label: {
+            HStack (spacing: 20){
+                ImageURL(url: drinkThumb)
+                //add here properties
+                                             // image.resizable()
+                                             //.aspectRatio(contentMode: .fit)
+                                            // .frame(maxWidth: 80)
+                
+                
+//                 AsyncImage(url: drinkThumb, content: { image in
+//                                     image.resizable()
+//                                              .aspectRatio(contentMode: .fit)
+//                                              .frame(maxWidth: 80)
+//                                     }, placeholder: {
+//                                         ProgressView()
+//                                     })
+//                 .clipShape(Circle())
+//                 .shadow(radius: 5)
+                
+                Text(drinkName)
+                    .font(.body).bold()
+            }
+            .padding(.vertical, 5)
+            .sheet(isPresented: $isSheetDisplayed) {
+                
+                //SECOND SCREEN
+                ForEach(drinkDetailVM.drinkDetail, id: \.idDrink) { drink in
+                    ScrollView {
+                        VStack {
+                            Capsule()
+                                .foregroundColor(.secondary)
+                                .opacity(0.5)
+                                .frame(width: 35, height: 5)
+                                .padding(5)
+                            
+                            AsyncImage(url: drink.strDrinkThumb, content: { image in
+                                                image.resizable()
+                                                         .aspectRatio(contentMode: .fit)
+                                                         .frame(maxWidth: 300)
+                                                }, placeholder: {
+                                                    ProgressView()
+                                                })
+                            .cornerRadius(25)
+                            .shadow(radius: 5)
+                            .padding()
+                            
+                            Text(drink.strDrink)
+                                .font(.title).bold()
+                                .padding()
+                            
+                            Text(drink.strInstructions)
+                                .font(.caption)
+                                .padding(.horizontal)
+                            
+                            Text("Ingredients")
+                                .font(.title2)
+                                .padding(.top, 5)
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                  ForEach(drinkDetailVM.arrayOfIngredients, id: \.self) {ingredient in
+                                       ForEach(drinkDetailVM.arrayOfMeasure, id: \.self) {measure in
+                                            VStack{
+                                                Text(ingredient)
+                                                Text(measure)
+                                            }
+                                        }
+                                  }
+                                       
+
+                                    .foregroundColor(.black)
+                                    .padding(10)
+                                    .background(Color("LightGray"))
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                    .shadow(radius: 5)
+                                }
+                                
+                                .padding(10)
+                            }
+                        }
+                    }
+                }
+                .task {
+                    await drinkDetailVM.getDrinkDetail(drinkID: drinkIDs)
+                }
+            }
+        }
+    }
+}
