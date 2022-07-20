64a80a564866329f67e7a26233a8e4a67efdc490
Damian Rydzewski <107407005+damianrydzewski@users.noreply.github.com>
2022-07-20 10:49:58 +0200

    Added arrays of strMeasure and strIngredients

--- DrinksApp/ViewModel/DrinkListViewModel.swift
+++ DrinksApp/ViewModel/DrinkListViewModel.swift
@@ -74,94 +74,38 @@ struct DrinkDetailVM {
     var strDrinkThumb: URL? {
         URL(string: drink.strDrinkThumb ?? "")
     }
-    var strIngredient1: String {
-        drink.strIngredient1 ?? ""
+    var arrayOfIngredients: [String?] {
+        return [drink.strIngredient1,
+                drink.strIngredient2,
+                drink.strIngredient3,
+                drink.strIngredient4,
+                drink.strIngredient5,
+                drink.strIngredient6,
+                drink.strIngredient7,
+                drink.strIngredient8,
+                drink.strIngredient9,
+                drink.strIngredient10,
+                drink.strIngredient11,
+                drink.strIngredient12,
+                drink.strIngredient13,
+                drink.strIngredient14,
+                drink.strIngredient15]
     }
-    var strIngredient2: String {
-        drink.strIngredient2 ?? ""
-    }
-    var strIngredient3: String {
-        drink.strIngredient3 ?? ""
-    }
-    var strIngredient4: String {
-        drink.strIngredient4 ?? ""
-    }
-    var strIngredient5: String {
-        drink.strIngredient5 ?? ""
-    }
-    var strIngredient6: String {
-        drink.strIngredient6 ?? ""
-    }
-    var strIngredient7: String {
-        drink.strIngredient7 ?? ""
-    }
-    var strIngredient8: String {
-        drink.strIngredient8 ?? ""
-    }
-    var strIngredient9: String {
-        drink.strIngredient9 ?? ""
-    }
-    var strIngredient10: String {
-        drink.strIngredient10 ?? ""
-    }
-    var strIngredient11: String {
-        drink.strIngredient11 ?? ""
-    }
-    var strIngredient12: String {
-        drink.strIngredient12 ?? ""
-    }
-    var strIngredient13: String {
-        drink.strIngredient13 ?? ""
-    }
-    var strIngredient14: String {
-        drink.strIngredient14 ?? ""
-    }
-    var strIngredient15: String {
-        drink.strIngredient15 ?? ""
-    }
-    var strMeasure1: String {
-        drink.strMeasure1 ?? ""
-    }
-    var strMeasure2: String {
-        drink.strMeasure2 ?? ""
-    }
-    var strMeasure3: String {
-        drink.strMeasure3 ?? ""
-    }
-    var strMeasure4: String {
-        drink.strMeasure4 ?? ""
-    }
-    var strMeasure5: String {
-        drink.strMeasure5 ?? ""
-    }
-    var strMeasure6: String {
-        drink.strMeasure6 ?? ""
-    }
-    var strMeasure7: String {
-        drink.strMeasure7 ?? ""
-    }
-    var strMeasure8: String {
-        drink.strMeasure8 ?? ""
-    }
-    var strMeasure9: String {
-        drink.strMeasure9 ?? ""
-    }
-    var strMeasure10: String {
-        drink.strMeasure10 ?? ""
-    }
-    var strMeasure11: String {
-        drink.strMeasure11 ?? ""
-    }
-    var strMeasure12: String {
-        drink.strMeasure12 ?? ""
-    }
-    var strMeasure13: String {
-        drink.strMeasure13 ?? ""
-    }
-    var strMeasure14: String {
-        drink.strMeasure14 ?? ""
-    }
-    var strMeasure15: String {
-        drink.strMeasure15 ?? ""
+    var arrayOfMeasure: [String?] {
+        return [drink.strMeasure1,
+                drink.strMeasure2,
+                drink.strMeasure3,
+                drink.strMeasure4,
+                drink.strMeasure5,
+                drink.strMeasure6,
+                drink.strMeasure7,
+                drink.strMeasure8,
+                drink.strMeasure9,
+                drink.strMeasure10,
+                drink.strMeasure11,
+                drink.strMeasure12,
+                drink.strMeasure13,
+                drink.strMeasure14,
+                drink.strMeasure15]
     }
 }
